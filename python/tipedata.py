2df9da79ecc50630f691806cd8a5e09a1d7323bb
ZxGvy <rindanmaulana15@gmail.com>
2022-10-27 09:38:20 +0530

    Menambahkan latihan
    Menambahkan latihan Algoritma dan Pemrograman

---
+++ python/tipedata.py
@@ -0,0 +1,38 @@
+print("---BELAJAR TIPE DATA---")
+
+#tipe data integers
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#tipe data flloating numbers
+a = 7.5
+print(a)
+
+#menggunakan single qoute
+myFirstName = "Rindan"
+
+#menggunakan double qoute
+myLastName = "Maulana"
+
+#penulisan string multiline
+myAge = "17"
+
+# Penulisan string multiline
+myHobby = """Hobi saya adalah Travelling. """
+
+# Mencetak 2 variabel string dengan perintah print
+print(myFirstName + " " + myLastName)
+print("Umur saya " + myAge)
+print("Hobby saya " + myHobby)
+
+# Contoh penulisan boolean yang benar
+a = True
+b = False
+
+# Contoh penulisan boolean yang salah
+# Jika dijalankan, pada baris ini akan error
+c = a
+
+print(c)
